561391c4ce554e753f2870ad5cb5f2a974cf429f
Stashia Seymour <stashiaseymour2@gmail.com>
2025-01-30 14:49:02 -0500

    Added a temperature check function

--- app.py
+++ app.py
@@ -16,3 +16,34 @@ def potential_divider(voltage, resistors):
 
 # Test the voltage divider function
 potential_divider(9, [3000, 1000])
+
+
+
+def temperature_check(temperature, unit):
+    # Statements with conditions for temperature check in Celsius
+    if unit == "C":
+        if temperature < 35:
+            print("the patient is hypothermic")
+        elif temperature > 38:
+            print("the patient is hyperthermic")
+        else:
+            print("the patient's temperature is normal")
+    
+    # Statements with conditions for temerature check in Fahrenheit
+    elif unit == "F":
+        if temperature < 95:
+            print("the patient is hypothermic")
+        elif temperature > 100:
+            print("the patient is hyperthermic")
+        else:
+            print("the patient's temperature is normal")
+    
+    else:
+        print("Invalid unit. Please use 'C' for Celsius or 'F' for Fahrenheit.")
+
+# Test the temperature check 
+temperature_check(14, "C")  # "the patient is hypothermic"
+temperature_check(37, "C")  # "the patient's temperature is normal"
+temperature_check(37, "F")  # "the patient is hypothermic"
+
+

51851049d52fb5ab2abfff7c436500bee9694163
Stashia Seymour <stashiaseymour2@gmail.com>
2025-01-30 14:25:41 -0500

    Added a parallel function

---
+++ app.py
@@ -0,0 +1,9 @@
+def parallel(resistors):
+    total = 0
+    for r in resistors:
+        total += 1 / r
+    effective_resistance = 1 / total
+    print(f"{effective_resistance:.3f} ohms") 
+
+# Testing the function
+parallel([330, 1000, 2200])

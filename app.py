64efc868e817e5ee2bb0fa3989410cc24481d460
Stashia Seymour <stashiaseymour2@gmail.com>
2025-01-30 14:30:51 -0500

    Added the voltage divider function

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ def parallel(resistors):
     effective_resistance = 1 / total
     print(f"{effective_resistance:.3f} ohms") 
 
-# Testing the function
+# Testing the parallel function
 parallel([330, 1000, 2200])
+
+def potential_divider(voltage, resistors):
+    total_resistance = sum(resistors)  
+    for resistor in resistors:
+        voltage_drop = (resistor / total_resistance) * voltage  
+        print(f"{voltage_drop:.2f}v")  
+
+# Test the voltage divider function
+potential_divider(9, [3000, 1000])
